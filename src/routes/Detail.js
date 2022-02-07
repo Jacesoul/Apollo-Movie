56e8a5de6a479e732ed76b5ecd5b6b73b85bb0f6
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-07 18:00:41 +0900

    Data & Apollo Dev Tools

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -26,6 +26,7 @@ const Container = styled.div`
 
 const Column = styled.div`
   margin-left: 10px;
+  width: 50%;
 `;
 
 const Title = styled.h1`
@@ -45,7 +46,9 @@ const Description = styled.p`
 const Poster = styled.div`
   width: 25%;
   height: 60%;
-  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
 `;
 
 export default () => {
@@ -54,11 +57,19 @@ export default () => {
   return (
     <Container>
       <Column>
-        <Title>Name</Title>
-        <Subtitle>English</Subtitle>
-        <Description>Lorem ipslalalala</Description>
+        <Title>{loading ? "Loading..." : data.movie.title}</Title>
+        {!loading && data.movie && (
+          <>
+            <Subtitle>
+              {data.movie.language} · {data.movie.rating}
+            </Subtitle>
+            <Description>{data.movie.description_intro}</Description>
+          </>
+        )}
       </Column>
-      <Poster></Poster>
+      <Poster
+        bg={data && data.movie ? data.movie.medium_cover_image : ""}
+      ></Poster>
     </Container>
   );
 };
