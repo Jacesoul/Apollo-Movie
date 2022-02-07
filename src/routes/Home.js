7b4061228dc21c0d1508b908796f14792034f4f5
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-07 15:12:56 +0900

    GET MOVIES Query part One

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
-function Home() {
-  return <h1>Home</h1>;
-}
-export default Home;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
